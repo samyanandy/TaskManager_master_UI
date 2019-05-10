220f2fac18c0cfbf876e457b513ffee207a3e944
samyanandy <49272565+samyanandy@users.noreply.github.com>
2019-05-10 15:36:18 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,50 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+import { ViewTaskComponent } from './components/view-task/view-task.component';
+import { HeaderComponent } from './components/header/header.component';
+import {TaskService} from './task.service';
+import {HttpModule} from '@angular/http';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule ,ReactiveFormsModule} from '@angular/forms';
+import {TaskFilter} from './task-filter';
+import { AddUserComponent } from './components/add-user/add-user.component';
+import { AddProjectComponent } from './components/add-project/add-project.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { ModalContentComponent } from './components/modal-content/modal-content.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddTaskComponent,
+    ViewTaskComponent,
+    HeaderComponent,
+    TaskFilter,
+    AddUserComponent,
+    AddProjectComponent,
+    ModalContentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    HttpModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgbModule,
+  
+  ],
+  providers: [
+    TaskService,
+
+  ],
+  bootstrap: [AppComponent],
+  entryComponents: [ ModalContentComponent ]
+
+})
+export class AppModule { }
